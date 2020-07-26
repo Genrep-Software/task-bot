9aeda5aa5855928a5161eb53c679cfcebc951b4b
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-07-26 12:57:30 -0400

    Fetch records from Airtable

--- task_bot.py
+++ task_bot.py
@@ -28,7 +28,7 @@ def load_api_keys(keyfile: str) -> Dict[str, str]:
     with open(keyfile, "r") as f:
         keys = json.load(f)
 
-    for key in ["bot_id", "group_id"]:
+    for key in ["bot_id", "airtable_api_key", "airtable_api_url"]:
         if key not in keys or not keys.get(key):
             raise KeyError(f"\"{key}\" not in key file {keyfile}")
 
@@ -45,16 +45,17 @@ class GroupmeBot(object):
     Represents a GroupMe bot that can handle sending and receiving messages.
     """
     bot_id: str
-    group_id: str
 
     POST_URL = "https://api.groupme.com/v3/bots/post"
+    MAX_MSG_LEN = 998
 
     def send(self, msg: str) -> None:
         """
         Send a message from the bot in the group.
         :param msg: Message to send; cuts into substrings of length 998
         """
-        for substr in [msg[i:(i+998)] for i in range(0, len(msg), 998)]:
+        for substr in [msg[i:(i + GroupmeBot.MAX_MSG_LEN)] for i in
+                       range(0, len(msg), GroupmeBot.MAX_MSG_LEN)]:
             data = {
                 "bot_id": self.bot_id,
                 "text": substr,
@@ -63,14 +64,54 @@ class GroupmeBot(object):
             time.sleep(0.5)
 
 
+class Airtable(object):
+    """
+    Represents the records in the Airtable tasks list.
+    """
+    def __init__(self, api_key: str, api_url: str):
+        self.api_url = api_url
+        self.session = requests.Session()
+        self.session.headers.update({
+            "Authorization": f"Bearer {api_key}"
+        })
+        self.records = dict()
+        self.get_records()
+
+    def get_records(self, view: str = "Bot") -> None:
+        # Get the first 100 records from the task list
+        params = tuple({
+            "view": view,
+        }.items())
+        with self.session.get(self.api_url, params=params) as r:
+            records = map(lambda i: i.get("fields"), r.json().get("records"))
+
+        # Add each of them to the object's dictionary of records
+        for record in records:
+            if record is None:
+                continue
+
+            assigned_to = record\
+                .get("Assigned To", dict())\
+                .get("name", "Unassigned")
+            if assigned_to not in self.records:
+                self.records[assigned_to] = []
+            self.records.get(assigned_to).append(record)
+
+
 ################################################################################
 # Main Function
 ################################################################################
 
 def main():
     keys = load_api_keys("api_keys.json")
-    bot = GroupmeBot(keys.get("bot_id"), keys.get("group_id"))
-    bot.send("Testy buddy!")
+    bot = GroupmeBot(keys.get("bot_id"))
+    # try:
+    table = Airtable(keys.get("airtable_api_key"), keys.get("airtable_api_url"))
+    print(json.dumps(table.records, indent=2))
+    """
+    except Exception as e:
+        bot.send(str(e))
+    """
 
 
 if __name__ == "__main__":
